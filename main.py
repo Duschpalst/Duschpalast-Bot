383e73cb97cd27e01e8ee6f2671329a61f24cf8a
Lauvxcz <laurenz.kellerhoff@gmx.de>
2024-05-15 12:05:48 +0200

    Updated Main

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import sys
 
 import discord
 
-
 # Files Import
 import secret
 import static
@@ -24,30 +23,35 @@ create_sql_tables()
 sys.stderr.write = MyErrorHandler"""
 
 
-
 @client.event
 async def on_ready():
     print("Ich bin on!")
 
-    # Code der nur aufm Main Bot funktioniert und nicht auf dem Test Bot
+    # Code that only works on the Main Bot and not the Test Bot
     if client.user.id == static.bot_id:
         await update_invites(client)
 
 
-
 cogs_directory = ['src']
 
-# Import all commands Files
+# Import all cogs Files
 for directory in cogs_directory:
     for path, subdirs, files in os.walk(directory):
         for name in files:
             if name.endswith(".py"):
+                # Replacing directory separator based on the operating system
                 if platform.system() == 'Linux':
-                    p = path.replace("/", ".") # Linux
+                    p = path.replace("/", ".")  # Linux
                 else:
-                    p = path.replace("\\", ".") # Windows
+                    p = path.replace("\\", ".")  # Windows or other
 
                 client.load_extension(f"{p}.{name[:-3]}")
 
 
-client.run(secret.Test_bot_TOKEN)
+# Run the Bot or the Test Bot based on the operating system
+if platform.system() == 'Linux':
+    TOKEN = secret.TOKEN  # Linux
+else:
+    TOKEN = secret.Test_bot_TOKEN  # Windows or other
+
+client.run(TOKEN)
